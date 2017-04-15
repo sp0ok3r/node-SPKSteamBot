59884d1b7fbb738a60afc0a2b11bbf166c472c8d
sp0ok3r <digitalfoxanon@hotmail.com>
2017-04-15 21:30:09 +0100

    Create savedbots.js

---
+++ savedbots.js
@@ -0,0 +1,7 @@
+{
+	"AllBots:": "...",
+	
+  "If you got multiple bots, you put here there steamids, they don't gonna spam each other": "...",
+  
+  "BOTS" : "76561198152342467,76561198308740836"
+}
