eadcdcfc7f890fadcbbdcb85317d34f9eedb3126
sp0ok3r <digitalfoxanon@hotmail.com>
2017-04-15 21:33:19 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,28 @@
+{
+	"AccOptions:": "...",
+	
+	"Phone_secret" : "0",
+	"Admin" : "76561198041931474",
+    "username" : "your_username",
+    "password" : "your_password",
+	
+	"IdlerOptions:": "...",
+	
+	"StartOnlineOROffline" : "1",
+	"WhenLogonStartIdlingAllGames" : "1",
+	"LogonIdlingNonSteam": "0",
+	"AllFunctions" : "1", 
+	
+	"GameOptions" : "...",
+  
+	"Games2Idle" : "753", 
+  "MultipleGamesExample" : "753,10,730,xx,xxx,xxx,xx", 
+ 
+	"MessagesOptions:": "...",
+	
+	"AutoAFKMsgActivate" : "0",
+	"AutoAFKMsg" : "SP0OK3R is away, dont try to speak to him right now, try later",
+	"AutoFriendAcceptActivate" : "0",
+	"AutoMsgWhenFriendAccept" : "Hello im a automated bot called Alfredo, if you want to speak to SP0OK3R, try later.."
+
+}
